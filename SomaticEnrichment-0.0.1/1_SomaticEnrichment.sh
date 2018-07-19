47d982e38a05cd8bfb08a580de1193bc6782588b
Chris Medway <chrismedway@Matthews-iMac.local>
2018-07-19 16:39:14 +0100

    add cnv scripts

--- SomaticEnrichment-0.0.1/1_SomaticEnrichment.sh
+++ SomaticEnrichment-0.0.1/1_SomaticEnrichment.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+#PBS -l walltime=20:00:00
+#PBS -l ncpus=12
+#PBS -l nodes=comp05
 set -euo pipefail
 
-# Description: Somatic Enrichment Pipeline
-# Author:      Christopher Medway, All Wales Medical Genetics Service
+PBS_O_WORKDIR=(`echo $PBS_O_WORKDIR | sed "s/^\/state\/partition1//"`)
+cd $PBS_O_WORKDIR
+
+# Description: Somatic Enrichment Pipeline. Requires fastq file split by lane
+# Author:      Christopher Medway, All Wales Medical Genetics Service. Includes code from GermlineEnrichment-2.5.2
 # Mode:        BY_SAMPLE
-# Use:         XXXX
+# Use:         bash within sample directory
 
 version="0.0.1"
 
@@ -12,7 +18,10 @@ version="0.0.1"
 . ./*.variables
 
 # load pipeline variables
-. /home/cm/"$pipelineName"/"$pipelineName"-"$pipelineVersion"/"$panel"/"$panel".variables
+. /data/diagnostics/pipelines/"$pipelineName"/"$pipelineName"-"$pipelineVersion"/"$panel"/"$panel".variables
+
+# path to panel capture bed file
+vendorCaptureBed=/data/diagnostics/pipelines/"$pipelineName"/"$pipelineName"-"$pipelineVersion"/"$panel"/"$panel"_ROI_b37.bed
 
 # define fastq variables
 for fastqPair in $(ls "$sampleId"_S*.fastq.gz | cut -d_ -f1-3 | sort | uniq)
@@ -23,38 +32,79 @@ for fastqPair in $(ls "$sampleId"_S*.fastq.gz | cut -d_ -f1-3 | sort | uniq)
     read2Fastq=$(ls "$fastqPair"_R2_*fastq.gz)
 
     # cutadapt
-#    ./lib/cutadapt.sh $fastqPair
+    ./lib/cutadapt.sh \
+        $seqId \
+        $sampleId \
+        $laneId \
+        $read1Fastq \
+        $read2Fastq \
+        $read1Adapter \
+       $read2Adapter
 
-    # fastqc
-#    ./lib/fastqc.sh $seqId $sampleId $laneId
+#    # fastqc
+    ./lib/fastqc.sh $seqId $sampleId $laneId
 
-    # fastq to ubam
-#    ./lib/fastq_to_ubam.sh $seqId $sampleId $laneId $worklistId $panel $expectedInsertSize
+     # fastq to ubam
+    ./lib/fastq_to_ubam.sh \
+        $seqId \
+        $sampleId \
+        $laneId \
+        $worklistId \
+        $panel \
+        $expectedInsertSize
 
     # bwa
-#    ./lib/bwa.sh $seqId $sampleId $laneId
+    ./lib/bwa.sh $seqId $sampleId $laneId
     
     done
 
 # merge & mark duplicate reads
-#./lib/mark_duplicates.sh $seqId $sampleId 
+./lib/mark_duplicates.sh $seqId $sampleId 
 
 # basequality recalibration
-#if [ "$includeBQSR = true" ] ; then
-#    echo "performing base quality recalibration"
-#    ./lib/bqsr.sh $seqId $sampleId $pipelineName $version $panel
-#else
-#    echo "skipping base quality recalibration"
-#    cp "$seqId"_"$sampleId"_rmdup.bam "$seqId"_"$sampleId".bam
-#    cp "$seqId"_"$sampleId"_rmdup.bai "$seqId"_"$sampleId".bai
-#fi
+if [ "$includeBQSR = true" ] ; then
+    ./lib/bqsr.sh $seqId $sampleId $pipelineName $version $panel
+else
+    echo "skipping base quality recalibration"
+    cp "$seqId"_"$sampleId"_rmdup.bam "$seqId"_"$sampleId".bam
+    cp "$seqId"_"$sampleId"_rmdup.bai "$seqId"_"$sampleId".bai
+fi
 
+
+# post-alignment QC
+./lib/post_alignment_qc.sh \
+    $seqId \
+    $sampleId \
+    $panel \
+    $minimumCoverage \
+    $vendorCaptureBed \
+    $padding \
+    $minBQS \
+    $minMQS
+
+# coverage calculations
+./lib/calculateCoverage.sh \
+    $seqId \
+    $sampleId \
+    $panel \
+    $minimumCoverage \
+    $vendorCaptureBed \
+    $padding \
+    $minBQS \
+    $minMQS
 
 # variant calling
-#./lib/mutect2.sh $seqId $sampleId $pipelineName $version $panel
+./lib/mutect2.sh $seqId $sampleId $pipelineName $version $panel $padding $minBQS $minMQS
 
 # variant filter
-#./lib/variant_filter.sh $seqId $sampleId
+./lib/variant_filter.sh $seqId $sampleId
 
 # annotation
 ./lib/annotation.sh $seqId $sampleId
+
+
+## CNV ANALYSIS
+
+# GATK4 somatic-CNV best practices
+bash GATK4_somaticCNV.sh $seqId $sampleId $panel $ROI
+
